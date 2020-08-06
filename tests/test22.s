49a3ee0cd7a8fef99fca986fdfd8d99086eeaa74
Emad Jacob Maroun <emoun.open@gmail.com>
2020-08-06 13:27:48 +0200

    Corrected tests 22 and 23.
    test22.s would previously read the UART status flag once,
    then entered a loop where the flag value is checked to see
    if the UART has some input ready for the program. However, this loop
    did not refresh the flag value, which meant if the first flag read
    didn't signal that some input was ready, the loop would be inifinate.
    This change correctly refreshes the flag value in every loop.
    
    test23.s had a loop that has the same fundamental problem as with test22.
    However, pasim's UART implementation always accepts output to the UART
    from the program, meaning the UART status flag is always the same.
    This change updates the test to just check the flag once, and then doesn't
    output '!' if the flag is not signalling that the UART is ready for output.

--- tests/test22.s
+++ tests/test22.s
@@ -1,4 +1,6 @@
 #
+# Continualy checks the status flag of the UART until
+# it signals there is some input, which is then loaded into r1.
 # Expected Result: r1 = 0xfb
 #
 
@@ -6,14 +8,14 @@
                 add     r1  = r0, 0xF0080000;
                 addi    r2  = r0, 2;
                 lwl     r3  = [r1 + 0];
-		nop;
+                nop;
                 andi    r3  = r3, 2;
 		cmpneq  p1 = r3, r2;
-           (p1) br      -3;
+           (p1) br      -4;
                 nop;
                 nop;
                 lwl     r1  = [r1 + 1];
                 halt;
-		nop;
-		nop;
-		nop;
+                nop;
+                nop;
+                nop;
--- tests/test23.s
+++ tests/test23.s
@@ -1,20 +1,20 @@
 #
+# Checks the UART status flag for whether it is ready to 
+# receive data. If so, the program outputs '!' to the UART 
+# (which should be seen in stdout.)
 # Expected Result: '!' is output, and r2 = 0x21
 #
 
-                .word   68;
+                .word   56;
                 add     r1  = r0, 0xF0080000;
                 addi    r2  = r0, 1;
                 lwl     r3  = [r1 + 0];
-		nop;
+                nop;
                 andi    r3  = r3, 1;
 		cmpneq  p1 = r3, r2;
-          (p1)  br      -3;
-                nop;
-                nop;
-                addi    r2 = r0, 0x21;
-                swl     [r1 + 1] = r2;
+          (!p1) addi    r2 = r0, 0x21;
+          (!p1) swl     [r1 + 1] = r2;
                 halt;
-		nop;
-		nop;
-		nop;
+                nop;
+                nop;
+                nop;
