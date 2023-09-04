3042446edf5e688330ac0bab7453a1a8ee019ba6
Emad Jacob Maroun <emoun.open@gmail.com>
2023-09-04 15:23:37 +0200

    pasim data-cache kind help message no longer misrepresents the defualt.

--- src/pasim.cc
+++ src/pasim.cc
@@ -399,7 +399,7 @@ int main(int argc, char **argv)
   cache_options.add_options()
     ("dcsize,d", boost::program_options::value<patmos::byte_size_t>()->default_value(patmos::NUM_DATA_CACHE_BYTES), "data cache size in bytes")
     ("dckind,D", boost::program_options::value<patmos::set_assoc_cache_type>()->default_value(patmos::set_assoc_cache_type(patmos::SAC_DM,1)),
-                 "kind of direct mapped/fully-/set-associative data cache, defaults to lru2 (ideal, no, dm, lru[N], fifo[N])")
+                 "kind of direct mapped/fully-/set-associative data cache (ideal, no, dm, lru[N], fifo[N])")
     ("dlsize",   boost::program_options::value<patmos::byte_size_t>()->default_value(0), "size of a data cache line in bytes, defaults to burst size if set to 0")
 
     ("scsize,s", boost::program_options::value<patmos::byte_size_t>()->default_value(patmos::NUM_STACK_CACHE_BYTES), "stack cache size in bytes")
