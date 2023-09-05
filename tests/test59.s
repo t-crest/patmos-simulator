4b3edc3a68774a3c020fb87d5eeeb4ddaf458e6c
Emad Jacob Maroun <emoun.open@gmail.com>
2023-09-05 09:14:00 +0200

    '--permissive-dual-issue' now disallows two enabled load/store.
    This is because implemented allowing that in hardware would be difficult.

--- tests/test59.s
+++ tests/test59.s
@@ -1,18 +1,14 @@
 #
-# Tests can have store and load enabled at the same time
+# Tests two enabled loads and stores in the same bundle
 #
 
-                .word   60;
-				addi    r1 = r0, DUMMY1;
-				addi    r2 = r0, DUMMY2;
-				addi    r3 = r0, 456;
-				pand	p3 = p0, p0
-                (p3) swm  [r2] = r3 || (p0) lwm  r4 = [r1]		
-				lwm  r5 = [r2]					
+                .word   100;
+                addi	r1 = r0, dummy;
+				pand 	p7 = p0, p0;
+                (p7) 	lwc		r3 = [0] || (p7) swc		[0] = r2;
                 halt;
                 nop;
                 nop;
                 nop;
+dummy:          .word   0;
 
-DUMMY1:			.word 123;
-DUMMY2:			.word 0;
